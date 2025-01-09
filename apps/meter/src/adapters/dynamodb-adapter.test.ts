999f92d139b10fbe70e2b8155d1b31f4d7bcb9f7
Melle <118454433+mellevanderlinde@users.noreply.github.com>
2025-01-09 10:24:46 +0100

    Test DynamodbAdapter

---
+++ apps/meter/src/adapters/dynamodb-adapter.test.ts
@@ -0,0 +1,20 @@
+import { describe, expect, it } from "vitest";
+import { DynamodbAdapter } from "./dynamodb-adapter";
+
+describe("DynamodbAdapter", () => {
+  it("should throw an error if AWS credentials are missing", () => {
+    expect(() => {
+      new DynamodbAdapter("table-name");
+    }).toThrowError("AWS credentials are missing");
+
+    process.env.AWS_ACCESS_KEY_ID = "access-key-id";
+    expect(() => {
+      new DynamodbAdapter("table-name");
+    }).toThrowError("AWS credentials are missing");
+
+    process.env.AWS_SECRET_ACCESS_KEY = "secret-access-key";
+    expect(() => {
+      new DynamodbAdapter("table-name");
+    }).not.toThrowError();
+  });
+});
